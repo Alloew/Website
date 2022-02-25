69bae8827c2ee7be7f72674fcab32117bfac4e0a
Quinn135 <quinn.flynn.coop@gmail.com>
2022-02-25 16:01:19 -0600

    Update News.jsx

--- src/News.jsx
+++ src/News.jsx
@@ -3,8 +3,14 @@ export default function News() {
     <div className="News">
       <h1>Alloew 7 is out!</h1>
       <br />
-      <a href="//think.alloew.com/" className="a largeText">
-        THINK.alloew.com
+      <a href="//think.alloew.com/" className="a">
+        Think.Alloew.Com
+      </a>
+      <a href="//hi.alloew.com/" className="a">
+        Hi.Alloew.Com
+      </a>
+      <a href="//words.alloew.com/" className="a">
+        Words.Alloew.Com
       </a>
       <img className="logo" src="/alloew.png" alt="Alloew Logo" />
     </div>
