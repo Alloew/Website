c7ec46e09cc2a2b28291782e0fbcd32cf7bc99ea
QuinnC <59322843+Quinn135@users.noreply.github.com>
2022-08-26 12:36:35 -0500

    Update Other.jsx

--- src/Other.jsx
+++ src/Other.jsx
@@ -12,6 +12,7 @@ export default function Other() {
       <h2>People that have decided that they want their name to be on Alloew : </h2>
       <p>Julian</p>
       <p>Zach</p>
+      <br><h3>Type in chat below if you want to have your name here!</h3>
 
       <br />
       <hr style={{ "width": "90%" }} />
