f922899fe46e52315b48c58467a252f2078e04d7
Quinn Cooperstein <quinn.flynn.coop@gmail.com>
2022-09-02 13:08:43 -0500

    asd

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,16 +2,6 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p>Contact me at : <a href="mailto:QuinnC@Alloew.com">QuinnC@Alloew.com</a></p>
-      <div id="743859315">
-      <script type="text/javascript">
-          try {
-              window._mNHandle.queue.push(function (){
-                  window._mNDetails.loadTag("743859315", "970x90", "743859315");
-              })
-          }
-          catch (error) {}
-    </script>
-    </div>
     </div>
   );
 }
--- src/GalleryItem.jsx
+++ src/GalleryItem.jsx
@@ -1,51 +1,10 @@
-export default function GalleryItem(props) {
-  function popup() {
-    var background = document.createElement("div");
-    background.className = "background";
-    var main = document.createElement("div");
-    var image = document.createElement("img");
-    var download = document.createElement("a");
-    var exit = document.createElement("button");
-    exit.innerText = `✖`;
-    exit.classList.add("x");
-    download.classList.add("button");
-    download.download = props.title;
-    download.href = props.src;
-    download.innerText = "Download";
-    image.src = props.src;
-    main.innerText = props.title;
-
-    main.classList.add("previewItem");
-    main.classList.add("shadow");
-
-    main.appendChild(image);
-    main.appendChild(download);
-    main.appendChild(exit);
-
-    document.body.appendChild(background);
-    document.body.appendChild(main);
-
-    window.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
-        main.remove();
-        background.remove();
-      }
-    });
-
-    background.addEventListener("click", () => {
-      main.remove();
-      background.remove();
-    });
-
-    exit.addEventListener("click", () => {
-      main.remove();
-      background.remove();
-    });
-  }
-  return (
-    <div onClick={popup} className="shadow item">
-      <img src={props.src} />
-      {props.title}
-    </div>
-  );
+export default function GalleryItem(props){
+    return(
+        <div className="galleryFrame">
+            <div className="galleryItem">
+                <img src={props.img} alt={props.name} />
+            </div>
+            <button>Download</button>
+        </div>
+    )
 }
