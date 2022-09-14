15cb602d09bd02e8da637125ef9d4937629c11e4
QuinnC <59322843+Quinn135@users.noreply.github.com>
2022-09-14 15:00:00 -0500

    Ad

--- src/Games.jsx
+++ src/Games.jsx
@@ -8,6 +8,22 @@ export default function Games() {
   return (
     <div id="Main" className="Games">
       <h1>Games</h1>
+      <br></br>
+    
+      <script type="text/javascript">
+        atOptions = {
+          'key' : '980183fb59fca582e2dd47d3773b4e5e',
+          'format' : 'iframe',
+          'height' : 90,
+          'width' : 728,
+          'params' : {}
+        };
+        document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/980183fb59fca582e2dd47d3773b4e5e/invoke.js"></scr' + 'ipt>');
+      </script>
+    
+      <br />
+      <hr style={{ "width": "90%" }} />
+      <br />
 
       <Turboman />
 
