9c1ddb7d37ae824efef5838a6d09232ca5f26f21
QuinnC <59322843+Quinn135@users.noreply.github.com>
2022-09-19 14:45:30 -0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,17 @@
 import News from "./News";
-import InnerHTML from 'dangerously-set-html-content'
+// import InnerHTML from 'dangerously-set-html-content'
 
 export default function Home() {
-  const ad = `<script type="text/javascript">
-	atOptions = {
-		'key' : '980183fb59fca582e2dd47d3773b4e5e',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="https://www.highperformancedisplayformat.com/980183fb59fca582e2dd47d3773b4e5e/invoke.js"></script>`;
+//   const ad = `<script type="text/javascript">
+// 	atOptions = {
+// 		'key' : '980183fb59fca582e2dd47d3773b4e5e',
+// 		'format' : 'iframe',
+// 		'height' : 90,
+// 		'width' : 728,
+// 		'params' : {}
+// 	};
+// </script>
+// <script type="text/javascript" src="https://www.highperformancedisplayformat.com/980183fb59fca582e2dd47d3773b4e5e/invoke.js"></script>`;
 
 
   return (
@@ -19,7 +19,7 @@ export default function Home() {
       <h1>Welcome To Alloew!</h1>
       <p>The website were we make things for fun!</p>
       <br />
-      <InnerHTML html={ad}></InnerHTML>
+//       <InnerHTML html={ad}></InnerHTML>
       <br />
       <hr style={{ "width": "90%" }} />
       <br />
