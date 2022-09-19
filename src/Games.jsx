3d9130f0cf9d982fb19a7373bc9e1295ab22f9b1
QuinnC <59322843+Quinn135@users.noreply.github.com>
2022-09-19 14:45:45 -0500

    Update Games.jsx

--- src/Games.jsx
+++ src/Games.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import Turboman from "./Turboman";
-import InnerHTML from 'dangerously-set-html-content'
+// import InnerHTML from 'dangerously-set-html-content'
 
-export default function Games() {
-  var game = React.createElement("Turboman");
+// export default function Games() {
+//   var game = React.createElement("Turboman");
 
-  const ad = `<script type="text/javascript">
-	atOptions = {
-		'key' : '980183fb59fca582e2dd47d3773b4e5e',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="https://www.highperformancedisplayformat.com/980183fb59fca582e2dd47d3773b4e5e/invoke.js"></script>`;
+//   const ad = `<script type="text/javascript">
+// 	atOptions = {
+// 		'key' : '980183fb59fca582e2dd47d3773b4e5e',
+// 		'format' : 'iframe',
+// 		'height' : 90,
+// 		'width' : 728,
+// 		'params' : {}
+// 	};
+// </script>
+// <script type="text/javascript" src="https://www.highperformancedisplayformat.com/980183fb59fca582e2dd47d3773b4e5e/invoke.js"></script>`;
 
   return (
     <div id="Main" className="Games">
       <h1>Games</h1>
 
-      <InnerHTML html={ad} />
+//       <InnerHTML html={ad} />
       <span>We have ads now</span>
 
       <br />
