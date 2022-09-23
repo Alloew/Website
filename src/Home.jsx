3cbe232cc0a1dc83c78ff133facb0f8a0c22a0b9
QuinnC <59322843+Quinn135@users.noreply.github.com>
2022-09-23 11:23:51 -0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,6 @@ export default function Home() {
       <h1>Welcome To Alloew!</h1>
       <p>The website were we make things for fun!</p>
       <br />
-//       <InnerHTML html={ad}></InnerHTML>
       <br />
       <hr style={{ "width": "90%" }} />
       <br />
