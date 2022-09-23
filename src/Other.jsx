bf0011f2de6ea5a3349f1fb6976ccbb88f9f7010
QuinnC <59322843+Quinn135@users.noreply.github.com>
2022-09-23 11:23:37 -0500

    Update Other.jsx

--- src/Other.jsx
+++ src/Other.jsx
@@ -1,23 +1,11 @@
 import Youtube from "./Youtube";
-// import InnerHTML from 'dangerously-set-html-content'
 
 export default function Other() {
-//   const ad = `<script type="text/javascript">
-// 	atOptions = {
-// 		'key' : '980183fb59fca582e2dd47d3773b4e5e',
-// 		'format' : 'iframe',
-// 		'height' : 90,
-// 		'width' : 728,
-// 		'params' : {}
-// 	};
-// </script>
-// <script type="text/javascript" src="https://www.highperformancedisplayformat.com/980183fb59fca582e2dd47d3773b4e5e/invoke.js"></script>`;
 
 
   return (
     <div id="Main" className="Other">
       <h1>Other</h1>
-//       <InnerHTML html={ad}></InnerHTML>
       <hr style={{ "width": "90%" }} />
       <h2>Our Community</h2>
       <p>Lead Developer : Quinn</p>
