53f8af899168bea92612e68bacc245c97cb1edd4
QuinnC <59322843+Quinn135@users.noreply.github.com>
2022-09-23 10:50:06 -0500

    Update Games.jsx

--- src/Games.jsx
+++ src/Games.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 import Turboman from "./Turboman";
-// import InnerHTML from 'dangerously-set-html-content'
 
 export default function Games() {
-//   var game = React.createElement("Turboman");
-
-//   const ad = `<script type="text/javascript">
-// 	atOptions = {
-// 		'key' : '980183fb59fca582e2dd47d3773b4e5e',
-// 		'format' : 'iframe',
-// 		'height' : 90,
-// 		'width' : 728,
-// 		'params' : {}
-// 	};
-// </script>
-// <script type="text/javascript" src="https://www.highperformancedisplayformat.com/980183fb59fca582e2dd47d3773b4e5e/invoke.js"></script>`;
 
   return (
     <div id="Main" className="Games">
