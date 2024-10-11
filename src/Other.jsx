d3ecb266f29d2f6fb189b0145401fe330c56b98e
QuinnC <59322843+Quinn135@users.noreply.github.com>
2024-10-10 19:05:19 -0500

    Update Other.jsx

--- src/Other.jsx
+++ src/Other.jsx
@@ -6,15 +6,19 @@ export default function Other() {
       <h1>Other</h1>
       <hr style={{ width: "90%" }} />
       <h2>Our Community</h2>
-      <p>Lead Developer : Quinn</p>
+      <p>CEO : Quinn</p>
       <p>Lead Artist : Flynn</p>
-      <p>Artist and Map designer : Jacob</p>
+      <p>Manager : Jacob</p>
       <h2>
         People that have decided that they want their name to be on Alloew :{" "}
       </h2>
       <p>Julian</p>
       <p>Zach</p>
       <p>Caleb</p>
+      <h2>People who beg to be on this website really bad</h2>
+      <p>Elon Musk</p>
+      <h2>petitewhitegirl</h2>
+      <p>Roger Thomas-Ivy</p>
       {/* <br /><h3>Type in chat below if you want to have your name here!</h3> */}
 
       {/* <br />
